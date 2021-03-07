96e2b089f1d02023e0973340a5bfc80e6ff5eddf
sonota88 <yosiot8753@gmail.com>
2021-03-07 09:55:56 +0900

    refactor Names: Add Name (types.pric)

--- vgvm.rb
+++ vgvm.rb
@@ -583,7 +583,7 @@ if $PROGRAM_NAME == __FILE__
     $stdin_ = File.open(stdin_file, "rb")
   end
 
-  stack_size = 2_600_000
+  stack_size = 2_700_000
   mem = Memory.new(stack_size)
   vm = Vm.new(
     mem,
