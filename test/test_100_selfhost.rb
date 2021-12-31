a7a2567aba67479e729e87ef7698dc034e51aa2a
sonota88 <yosiot8753@gmail.com>
2021-12-31 12:49:32 +0900

    chore: Print running test file (test/test_100_selfhost.rb)

--- test/test_100_selfhost.rb
+++ test/test_100_selfhost.rb
@@ -22,6 +22,8 @@ class Test100 < Minitest::Test
     diff_cmd = "ruby " + project_path("selfhost/test/diff.rb")
 
     files.each do |file|
+      $stderr.puts "test #{file}:"
+
       file_src = project_path("selfhost/test/selfhost/#{file}")
 
       pricc_rb(  file_src, FILE_ASM_RB  , print_asm: true)
