2b22e31075de672f4f0efc215d3b36ac224bd2b0
sonota88 <yosiot8753@gmail.com>
2022-01-01 13:13:04 +0900

    Add test/test_110_return.rb

---
+++ test/test_110_return.rb
@@ -0,0 +1,28 @@
+require_relative "helper"
+
+class Test110 < Minitest::Test
+
+  def setup
+    setup_common()
+  end
+
+  # --------------------------------
+
+  def test_010
+    src = <<~SRC
+      def f()
+        return 65; # A
+        return 66; # B ... should not be executed
+      end
+
+      def main()
+        putchar(f());
+      end
+    SRC
+
+    output = run_vm(src)
+
+    assert_equal("A", output)
+  end
+
+end
