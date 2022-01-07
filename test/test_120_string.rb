428bf268e6db9f243c50d1a2146d9c5290459406
sonota88 <yosiot8753@gmail.com>
2022-01-08 08:59:51 +0900

    test: Add test/test_120_string.rb

---
+++ test/test_120_string.rb
@@ -0,0 +1,45 @@
+require_relative "helper"
+
+class Test120 < Minitest::Test
+  def setup
+    setup_common()
+
+    @std_src = File.read(
+      project_path("selfhost/lib/std.pric")
+    )
+  end
+
+  # --------------------------------
+
+  def test_010
+    src = <<~SRC
+      #{@std_src}
+
+      def GO_STRINGS() return GO_ALLOC_CURSOR() + GS_ALLOC_CURSOR(); end
+      def GS_STRINGS() return 10; end
+
+      def main()
+        var [11]g;
+        var g_ = &g;
+        var c_slash = 47;
+        var str_;
+
+        # init globals
+        init_alloc_cursor(g_);
+        init_strings(g_);
+
+        str_ = "fdsa";
+
+        print_i(str_size(str_));
+        putchar(c_slash);
+
+        print_s(str_);
+        putchar(c_slash);
+      end
+    SRC
+
+    output = run_vm(src)
+
+    assert_equal("4/fdsa/", output)
+  end
+end
